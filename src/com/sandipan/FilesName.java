574949f06c0033b4bb25fd0300831cb73329c23b
Sandipan basu <basusandipan0@gmail.com>
2021-08-08 18:07:59 +0530

    NIO

---
+++ src/com/sandipan/FilesName.java
@@ -0,0 +1,22 @@
+package com.sandipan;
+
+public final class FilesName {
+
+    private FilesName(){}
+
+    public final static String LOCATIONS = "locations.txt";
+
+    public final static String DIRECTIONS = "directions.txt";
+
+    public final static String DELIMITERS = ", ";
+
+    public final static String BUFFERED_WRITTEN_LOCATIONS = "buffered_locations.txt";
+
+    public final static String BUFFERED_WRITTEN_DIRECTIONS = "buffered_directions.txt";
+
+    public final static String BUFFERED_WRITTEN_LOCATION_OBJECT = "buffered_locations_object.dat";
+
+    public final static String RANDOM_ACCESS_LOCATION = "random_access_locations_object.dat";
+
+
+}
